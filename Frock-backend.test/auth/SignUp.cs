4c667f25131aa21da86eb3bc7fd48f52a923614c
Sr-Anonymus-maker <carlosleonrioja@gmail.com>
2025-07-11 04:24:44 -0500

    feat: sign up test

---
+++ Frock-backend.test/auth/SignUp.cs
@@ -0,0 +1,54 @@
+﻿using Frock_backend.IAM.Application.Internal.CommandServices;
+using Frock_backend.IAM.Application.Internal.OutboundServices;
+using Frock_backend.IAM.Domain.Model.Aggregates;
+using Frock_backend.IAM.Domain.Model.Commands;
+using Frock_backend.IAM.Domain.Repositories;
+using Frock_backend.shared.Domain.Repositories;
+using Moq;
+using TechTalk.SpecFlow;
+
+[Binding]
+public class SignUpSteps
+{
+    private readonly Mock<IUserRepository> _repo = new();
+    private readonly Mock<IUnitOfWork> _uow = new();
+    private readonly Mock<ITokenService> _tokenService = new();
+    private readonly Mock<IHashingService> _hashingService = new();
+    private UserCommandService _service;
+    private SignUpCommand _cmd;
+    private User _result;
+
+    [Given(@"no existe cuenta a crear")]
+    public void GivenNoExistingCompanie()
+    {
+        _service = new UserCommandService(_repo.Object, _tokenService.Object, _hashingService.Object, _uow.Object);
+    }
+
+    [When(@"envío los datos para la nueva cuenta:")]
+    public async Task WhenEnvioDatos(Table table)
+    {
+        var row = table.Rows[0];
+        _cmd = new SignUpCommand(
+            row["email"],
+            row["username"],
+            row["password"],
+            int.Parse(row["role"])
+        );
+        _result = await _service.Handle(_cmd);
+    }
+
+    [Then(@"el sistema crea una nueva cuenta")]
+    public void ThenUserCreated()
+        => Assert.NotNull(_result);
+
+    [Then(@"la cuenta tiene un Id numérico válido")]
+    public void ThenValidId()
+        => Assert.True(_result.Id > -1);
+
+    [Then(@"el nombre y correo de la cuenta creada coincide con lo ingresado")]
+    public void ThenFieldsMatch()
+    {
+        Assert.Equal(_cmd.Email, _result.Email);
+        Assert.Equal(_cmd.Username, _result.Username);
+    }
+}
---
+++ Frock-backend.test/auth/SignUp.feature
@@ -0,0 +1,13 @@
+﻿Feature: Crear cuenta
+    Como usuario quiero poder crear una cuenta
+    Quiero crear una cuenta
+    Para poder usar los servicios
+
+Scenario: Creacion exitosa de la cuenta
+    Given no existe cuenta a crear
+    When envío los datos para la nueva cuenta:
+    | email          | userName | password     | role |
+    | user@email.com | user1    | password1234 | 1    |
+    Then el sistema crea una nueva cuenta
+    And la cuenta tiene un Id numérico válido
+    And el nombre y correo de la cuenta creada coincide con lo ingresado
